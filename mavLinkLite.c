958ef70326e889d84ede528d2e165488497fc2e0
Royang <roydonghao@qq.com>
2019-09-03 09:56:19 +0800

    repo update

--- mavLinkLite.c
+++ mavLinkLite.c
@@ -1,51 +1,129 @@
+/*
+ * mavLinkLite.c
+ *
+ *  Created on: 2019-1-14
+ *      Author: zdh
+ */
+
 #include "mavLinkLite.h"
 #include "string.h"
+#include "stdio.h"
 
 void mavLinkLiteInit(mavLinkLiteHandle_t *mavLinkLiteHandle){
-	mavLinkLiteHandle->decodeStatus = searchHeader;
-	mavLinkLiteHandle->decodeFrameCharCnt = 0;
+	mavLinkLiteHandle->decodeFrameParseCharIndex = 0;
+	mavLinkLiteHandle->decodeFrameWriteInCharIndex = 0;
 }
 
 int mavLinkLitePayloadSend(mavLinkLiteHandle_t *mavLinkLiteHandle, uint8_t *buf, uint16_t len){
-	uint8_t frame[MAVLINK_PAYLOAD_LEN_MAX+4]={0};
+	uint8_t frame[MAVLINK_PAYLOAD_LEN_MAX + sizeof(mavLinkLiteHeader_t) + MAVLINK_CHECK_BYTE_LEN]={0};
 	uint16_t checkValue;
-	
+	mavLinkLiteHeader_t header;
+
 	if(len > MAVLINK_PAYLOAD_LEN_MAX){
 		return -1;
 	}
-	
-	frame[0] = MAVLINK_HEADER;
-	frame[1] = len;
-	memcpy(frame+2, buf , len);
-	checkValue = mavLinkLiteHandle->mavLinkLiteCheckFunc(frame, len+2);
-	frame[len+2] = (checkValue&0xff00) >> 8;
-	frame[len+3] = checkValue&0x00ff;
-	
-	mavLinkLiteHandle->mavLinkLiteSendFunc(frame, len+4);
-	
-	return 0;
+
+	header.frameHeader = MAVLINK_HEADER;
+	header.protocolVersion = MAVLINK_PROTOCOL_VERSION;
+	header.payloadLength = len;
+	memcpy(frame, &header, sizeof(mavLinkLiteHeader_t));
+
+	memcpy(frame+sizeof(mavLinkLiteHeader_t), buf , len);
+
+	checkValue = mavLinkLiteHandle->mavLinkLiteCheckFunc(frame, len+sizeof(mavLinkLiteHeader_t));
+	memcpy(frame+sizeof(mavLinkLiteHeader_t)+len, (uint8_t *)&checkValue, sizeof(checkValue));
+
+	return mavLinkLiteHandle->mavLinkLiteSendFunc(frame, len+sizeof(mavLinkLiteHeader_t)+sizeof(checkValue));
 }
 
-
-int  mavLinkLiteDecode(mavLinkLiteHandle_t *mavLinkLiteHandle, uint8_t *buf, uint16_t len){
+int mavLinkLiteDecodeStreamWriteIn(mavLinkLiteHandle_t *mavLinkLiteHandle, uint8_t *buf, uint16_t len){
 	uint16_t i=0;
-	
+
 	for(i=0; i<len; i++){
-		switch(mavLinkLiteHandle->decodeStatus){
-			case searchHeader:
-				if(buf[i] == MAVLINK_HEADER){
-					mavLinkLiteHandle->decodeFrameCharCnt = 0;
-					mavLinkLiteHandle->decodeFrame[mavLinkLiteHandle->decodeFrameCharCnt] = buf[i];
-				}
-				break;
-			case check:
-				
-				break;
-			default:
-				break;
+		mavLinkLiteHandle->decodeFrame[mavLinkLiteHandle->decodeFrameWriteInCharIndex++] = buf[i];
+		if(mavLinkLiteHandle->decodeFrameWriteInCharIndex == MAVLINK_CYCLE_BUFFER_LEN){
+			mavLinkLiteHandle->decodeFrameWriteInCharIndex = 0;
 		}
 	}
-	
+
+	return len;
+}
+
+int mavLinkLiteDecode(mavLinkLiteHandle_t *mavLinkLiteHandle){
+	uint16_t i=0;
+	uint16_t unparsedCharCnt=0;
+
+	//if cycle buffer write in index not equal to parsed index
+	if(mavLinkLiteHandle->decodeFrameWriteInCharIndex != mavLinkLiteHandle->decodeFrameParseCharIndex){
+		//calculate how many bytes need to parse
+		if(mavLinkLiteHandle->decodeFrameWriteInCharIndex > mavLinkLiteHandle->decodeFrameParseCharIndex){
+			unparsedCharCnt = mavLinkLiteHandle->decodeFrameWriteInCharIndex - mavLinkLiteHandle->decodeFrameParseCharIndex;
+		}else{
+			unparsedCharCnt = MAVLINK_CYCLE_BUFFER_LEN - mavLinkLiteHandle->decodeFrameParseCharIndex + mavLinkLiteHandle->decodeFrameWriteInCharIndex;
+		}
+		//unpared bytes at least have header and check bytes
+		if(unparsedCharCnt > sizeof(mavLinkLiteHeader_t) + MAVLINK_CHECK_BYTE_LEN){
+			//get frame header if not pass first byte 
+			if(mavLinkLiteHandle->decodeFrame[mavLinkLiteHandle->decodeFrameParseCharIndex] == MAVLINK_HEADER){
+				mavLinkLiteHeader_t *header;
+				uint8_t unparsedFrame[MAVLINK_PAYLOAD_LEN_MAX + sizeof(mavLinkLiteHeader_t) + MAVLINK_CHECK_BYTE_LEN]={0};
+
+				//copy header to buffer
+				for(i=0; i<sizeof(mavLinkLiteHeader_t); i++){
+					if(mavLinkLiteHandle->decodeFrameParseCharIndex + i >= MAVLINK_CYCLE_BUFFER_LEN){
+						unparsedFrame[i] = mavLinkLiteHandle->decodeFrame[mavLinkLiteHandle->decodeFrameParseCharIndex + i - MAVLINK_CYCLE_BUFFER_LEN];
+					}else{
+						unparsedFrame[i] = mavLinkLiteHandle->decodeFrame[mavLinkLiteHandle->decodeFrameParseCharIndex + i];
+					}
+				}
+				
+				header = (mavLinkLiteHeader_t *)unparsedFrame;
+				//check if length is legal, else, bad frame pass first byte
+				if(header->payloadLength < MAVLINK_PAYLOAD_LEN_MAX){
+					//check length is enough to parse 
+					if(unparsedCharCnt >= header->payloadLength + sizeof(mavLinkLiteHeader_t) + MAVLINK_CHECK_BYTE_LEN){
+						//copy whole frame to buffer
+						for(; i<header->payloadLength + sizeof(mavLinkLiteHeader_t) + MAVLINK_CHECK_BYTE_LEN; i++){
+							if(mavLinkLiteHandle->decodeFrameParseCharIndex + i >= MAVLINK_CYCLE_BUFFER_LEN){
+								unparsedFrame[i] = mavLinkLiteHandle->decodeFrame[mavLinkLiteHandle->decodeFrameParseCharIndex + i - MAVLINK_CYCLE_BUFFER_LEN];
+							}else{
+								unparsedFrame[i] = mavLinkLiteHandle->decodeFrame[mavLinkLiteHandle->decodeFrameParseCharIndex + i];
+							}
+						}
+						
+						//caculate check value
+						uint16_t calculateValue = mavLinkLiteHandle->mavLinkLiteCheckFunc(unparsedFrame, header->payloadLength + sizeof(mavLinkLiteHeader_t));
+						//get check value in frame 
+						uint16_t checkValue = unparsedFrame[header->payloadLength+sizeof(mavLinkLiteHeader_t)] | (unparsedFrame[header->payloadLength+sizeof(mavLinkLiteHeader_t)+1]<<8);
+
+						if(checkValue == calculateValue){
+							mavLinkLiteHandle->mavLinkLiteCommandIdParse(unparsedFrame, unparsedCharCnt);//command id parse
+							mavLinkLiteHandle->decodeFrameParseCharIndex += i;					//write in index pass whole frame
+							if(mavLinkLiteHandle->decodeFrameParseCharIndex >= MAVLINK_CYCLE_BUFFER_LEN){
+								mavLinkLiteHandle->decodeFrameParseCharIndex -= MAVLINK_CYCLE_BUFFER_LEN;
+							}
+						}else{//bad check value, pass first byte
+							mavLinkLiteHandle->decodeFrameParseCharIndex++;
+							if(mavLinkLiteHandle->decodeFrameParseCharIndex == MAVLINK_CYCLE_BUFFER_LEN){
+								mavLinkLiteHandle->decodeFrameParseCharIndex = 0;
+							}
+						}
+					}
+				}else{
+					mavLinkLiteHandle->decodeFrameParseCharIndex++;
+					if(mavLinkLiteHandle->decodeFrameParseCharIndex == MAVLINK_CYCLE_BUFFER_LEN){
+						mavLinkLiteHandle->decodeFrameParseCharIndex = 0;
+					}
+				}
+			}else{
+				mavLinkLiteHandle->decodeFrameParseCharIndex++;
+				if(mavLinkLiteHandle->decodeFrameParseCharIndex == MAVLINK_CYCLE_BUFFER_LEN){
+					mavLinkLiteHandle->decodeFrameParseCharIndex = 0;
+				}
+			}
+		}
+	}
+
 	return -1;
 }
 
--- mavLinkLite.h
+++ mavLinkLite.h
@@ -1,27 +1,43 @@
-#ifndef _MAVLINKLITE_H_
-#define _MAVLINKLITE_H_
+/*
+ * mavLinkLite.h
+ *
+ *  Created on: 2019-1-14
+ *      Author: zdh
+ */
+
+#ifndef MAVLINKLITE_H_
+#define MAVLINKLITE_H_
 
 #include "stdint.h"
 
-#define MAVLINK_HEADER		0xfe
-#define MAVLINK_PAYLOAD_LEN_MAX		32
-
-typedef enum {
-	searchHeader,
-	check
-}decodeStatus_e;
-	
+#define MAVLINK_HEADER							0xfe
+#define MAVLINK_PAYLOAD_LEN_MAX			64
+#define MAVLINK_CYCLE_BUFFER_LEN		128
+#define MAVLINK_PROTOCOL_VERSION		0
+#define MAVLINK_CHECK_BYTE_LEN			2
+#define MAVLINKLITE_DEBUG						0
 
 typedef struct{
-	decodeStatus_e decodeStatus;
-	
+	uint8_t frameHeader;
+	uint8_t protocolVersion;
+	uint16_t payloadLength;
+}mavLinkLiteHeader_t;
+
+typedef struct{
+	uint16_t decodeFrameParseCharIndex;
+	uint16_t decodeFrameWriteInCharIndex;
+	uint8_t decodeFrame[MAVLINK_CYCLE_BUFFER_LEN];
+
 	uint16_t (*mavLinkLiteCheckFunc)(uint8_t *buf, uint16_t len);
 	int (*mavLinkLiteSendFunc)(uint8_t *buf, uint16_t len);
-	uint8_t decodeFrame[MAVLINK_PAYLOAD_LEN_MAX+4];
-	uint16_t decodeFrameCharCnt;
+	void (*mavLinkLiteCommandIdParse)(uint8_t *buf, int len);
 }mavLinkLiteHandle_t;
 
+//the following functions have no measure about reentry, make sure there is no context during the function is execute
+void mavLinkLiteInit(mavLinkLiteHandle_t *mavLinkLiteHandle);
+int mavLinkLitePayloadSend(mavLinkLiteHandle_t *mavLinkLiteHandle, uint8_t *buf, uint16_t len);
+int mavLinkLiteDecodeStreamWriteIn(mavLinkLiteHandle_t *mavLinkLiteHandle, uint8_t *buf, uint16_t len);
+//make sure only one service function, this function do not have any measure about reentry 
+int mavLinkLiteDecode(mavLinkLiteHandle_t *mavLinkLiteHandle);
 
-
-#endif
-
+#endif /* MAVLINKLITE_H_ */
